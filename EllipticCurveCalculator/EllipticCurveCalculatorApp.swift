c9a9a398c1fc1d9a9a7ad8870a0ad0d6ce5e9fe1
Jorge Benjumea <me@intron014.com>
2024-03-30 17:53:42 +0100

    Initial Commit

---
+++ EllipticCurveCalculator/EllipticCurveCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EllipticCurveCalculatorApp.swift
+//  EllipticCurveCalculator
+//
+//  Created by Jorge Benjumea on 30/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EllipticCurveCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
